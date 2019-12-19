e1dfc1e65ce40f1eb7eb89c324afc43fc9eb11f4
linxiaojie <xiaojie.lin@mozi.top>
2019-12-19 17:57:14 +0800

    add list2 test code

--- test.c
+++ test.c
@@ -3,21 +3,73 @@
 #include <stdlib.h>
 
 #include "bit.h"
+#include "list2.h"
 
-int main(int argc, char *argv[])
+
+static void test_bit(void)
 {
-	int ret = 0;
-
 	Bit_T set = NULL;
 	set = MODULE_FUN_NAME(Bit, new)(36);
 	if (NULL == set)
 	{
-		return 0;
+		return ;
 	}
 	MODULE_FUN_NAME(Bit, set)(set, 5, 10);
 	fprintf(stdout, "set bit: %d\n", MODULE_FUN_NAME(Bit, count)(set));
 	MODULE_FUN_NAME(Bit, put)(set, 20, 1);
 	fprintf(stdout, "set bit: %d\n", MODULE_FUN_NAME(Bit, count)(set));
+
+}
+
+static void test_list2(void)
+{
+	List_T list = NULL;
+	ListNode_T node = NULL;
+
+	int i;
+
+	list = MODULE_FUN_NAME(List, new)();
+	if (NULL == list)
+		return ;
+
+	for (i = 0; i < 20; i++)
+	{
+		node = MODULE_FUN_NAME(ListNode, new)((void *)(i + 1));
+		MODULE_FUN_NAME(List, insert)(list, node);
+	}
+
+	for (i = MODULE_FUN_NAME(List, count)(list), 
+			node = MODULE_FUN_NAME(List, head)(list);
+			i > 0;
+			i--, node = MODULE_FUN_NAME(List, next)(node))
+	{
+		fprintf(stdout, "i: %d, value: %d\n", i, (int)node->priv);
+	}
+
+	MODULE_FUN_NAME(List, free)(&list);
+
+}
+
+struct test_routine {
+	void (*call_back)(void);
+};
+
+struct test_routine my_test_routines[] = 
+{
+		test_bit,
+		test_list2,
+		NULL,
+};
+
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	int i;
+
+	for (i = 0; my_test_routines[i].call_back != NULL; i++)
+	{
+		my_test_routines[i].call_back();
+	}
 
 	return ret;
 }
