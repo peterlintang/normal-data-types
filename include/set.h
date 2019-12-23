edb0e3c53696713fe9a574c94223ef2ba865bfa5
linxiaojie <xiaojie.lin@mozi.top>
2019-12-23 17:13:52 +0800

    add comments for set

--- include/set.h
+++ include/set.h
@@ -18,35 +18,122 @@
 #define T Set_T
 typedef struct T *T;
 
-/* 
- * interface: 
- * description:  
- * args: 
- * return value: 
+/*
+ * name: MODULE_FUN_NAME(Set, new)
+ * description: create a new set, 
+ * return value: return the pointer to new set
+ * args: @hint: number of elements in set suggested
+ * 		 @cmp: function to compare two elements
+ * 		 @hash: function do hash
  */
-// < functions 138 >
-extern T MODULE_FUN_NAME(Set, new)(int hint,
+T MODULE_FUN_NAME(Set, new)(int hint,
 			int cmp(const void *x, const void *y),
 			unsigned hash(const void *x));
-extern void MODULE_FUN_NAME(Set, free)(T *set);
 
-extern int MODULE_FUN_NAME(Set, length)(T set);
-extern int MODULE_FUN_NAME(Set, member)
+/*
+ * name: MODULE_FUN_NAME(Set, free)
+ * description: free a set
+ * return value: return void
+ * args: @set: pointer's pointer to set
+ */
+void MODULE_FUN_NAME(Set, free)(T *set);
+
+/*
+ * name: MODULE_FUN_NAME(Set, length)
+ * description: return total elements in set
+ * return value: return total number of elements in set
+ * args: @set: pointer to set
+ */
+int MODULE_FUN_NAME(Set, length)(T set);
+
+/*
+ * name: MODULE_FUN_NAME(Set, member)
+ * description: judge @member is in @set
+ * return value: 1: is a member of set
+ * 				 0: not
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
+int MODULE_FUN_NAME(Set, member)
 		(T set, const void *member);
-extern int MODULE_FUN_NAME(Set, put)
-		(T set, const void *member);
-extern void *MODULE_FUN_NAME(Set, remove)
+/*
+ * name: MODULE_FUN_NAME(Set, put)
+ * description: judge @member is in @set
+ * return value: 1: put success
+ * 				 0: failed to put in set
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
+int MODULE_FUN_NAME(Set, put)
 		(T set, const void *member);
 
-extern void MODULE_FUN_NAME(Set, map)(T set, 
+/*
+ * name: MODULE_FUN_NAME(Set, remove)
+ * description: remove @member from @set
+ * return value: pointer to member: success
+ * 				 NULL: failed 
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
+void *MODULE_FUN_NAME(Set, remove)
+		(T set, const void *member);
+
+/*
+ * name: MODULE_FUN_NAME(Set, map)
+ * description: for each member in @set, call @apply
+ * return value: void
+ * args: @set: pointer to set
+ * 		@apply: function to call
+ * 		@cl: private data to user
+ */
+void MODULE_FUN_NAME(Set, map)(T set, 
 				void apply(const void *member, void *cl),
 				void *cl);
-extern void **MODULE_FUN_NAME(Set, toArray)(T set, void *end);
 
-extern T MODULE_FUN_NAME(Set, union)(T s, T t);	// s + t
-extern T MODULE_FUN_NAME(Set, inter)(T s, T t);	// s * t
-extern T MODULE_FUN_NAME(Set, minus)(T s, T t);	// s - t 
-extern T MODULE_FUN_NAME(Set, diff)(T s, T t);	// s / t 
+/*
+ * name: MODULE_FUN_NAME(Set, toArray)
+ * description: put members in @set to array
+ * return value: array to members
+ * args: @set: pointer to set
+ * 		@end: indicator end of array
+ */
+void **MODULE_FUN_NAME(Set, toArray)(T set, void *end);
+
+/*
+ * name: MODULE_FUN_NAME(Set, union)
+ * description: put members in @s and @t to a new set
+ * return value: return a set, which the member is in t or in s 
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
+T MODULE_FUN_NAME(Set, union)(T s, T t);	// s + t
+
+/*
+ * name: MODULE_FUN_NAME(Set, inter)
+ * description: put members both in @s and @t to a new set
+ * return value: return a set, which the member is in t and in s 
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
+T MODULE_FUN_NAME(Set, inter)(T s, T t);	// s * t
+
+/*
+ * name: MODULE_FUN_NAME(Set, minus)
+ * description: put members in @s and not in @t to a new set
+ * return value: return a set, which the member is in t but not in s
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
+T MODULE_FUN_NAME(Set, minus)(T s, T t);	// s - t 
+
+/*
+ * name: MODULE_FUN_NAME(Set, diff)
+ * description:  put members which only in @s or in @t to a new set
+ * return value: return a set, the member only in s or only in t
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
+T MODULE_FUN_NAME(Set, diff)(T s, T t);	// s / t 
 
 #undef T
 
--- src/set.c
+++ src/set.c
@@ -75,6 +75,14 @@ static T copy(T t, int hint)
 
 
 // < functions 149>
+/*
+ * name: MODULE_FUN_NAME(Set, new)
+ * description: create a new set, 
+ * return value: return the pointer to new set
+ * args: @hint: number of elements in set suggested
+ * 		 @cmp: function to compare two elements
+ * 		 @hash: function do hash
+ */
 T MODULE_FUN_NAME(Set, new)(int hint, 
 				int cmp(const  void *x, const void *y),
 				unsigned hash(const void *x))
@@ -108,6 +116,14 @@ T MODULE_FUN_NAME(Set, new)(int hint,
 	return set;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, member)
+ * description: judge @member is in @set
+ * return value: 1: is a member of set
+ * 				 0: not
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
 int MODULE_FUN_NAME(Set, member)(T set, const void *member)
 {
 	int i;
@@ -125,6 +141,14 @@ int MODULE_FUN_NAME(Set, member)(T set, const void *member)
 	return p != NULL;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, put)
+ * description: judge @member is in @set
+ * return value: 1: put success
+ * 				 0: failed to put in set
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
 int MODULE_FUN_NAME(Set, put)(T set, const void *member)
 {
 	int i;
@@ -162,6 +186,14 @@ int MODULE_FUN_NAME(Set, put)(T set, const void *member)
 		return 0;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, remove)
+ * description: remove @member from @set
+ * return value: pointer to member: success
+ * 				 NULL: failed 
+ * args: @set: pointer to set
+ * 		@member: pointer to member
+ */
 void *MODULE_FUN_NAME(Set, remove)(T set, const void *member)
 {
 	int i;
@@ -189,6 +221,12 @@ void *MODULE_FUN_NAME(Set, remove)(T set, const void *member)
 	return NULL;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, length)
+ * description: return total elements in set
+ * return value: return total number of elements in set
+ * args: @set: pointer to set
+ */
 int MODULE_FUN_NAME(Set, length)(T set)
 {
 	assert(set);
@@ -196,6 +234,12 @@ int MODULE_FUN_NAME(Set, length)(T set)
 	return set->length;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, free)
+ * description: free a set
+ * return value: return void
+ * args: @set: pointer's pointer to set
+ */
 void MODULE_FUN_NAME(Set, free)(T *set)
 {
 	int i;
@@ -220,6 +264,14 @@ void MODULE_FUN_NAME(Set, free)(T *set)
 	*set = NULL;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, map)
+ * description: for each member in @set, call @apply
+ * return value: void
+ * args: @set: pointer to set
+ * 		@apply: function to call
+ * 		@cl: private data to user
+ */
 void MODULE_FUN_NAME(Set, map)
 	(T set, void apply(const void *member, void *cl), void *cl)
 {
@@ -241,6 +293,13 @@ void MODULE_FUN_NAME(Set, map)
 	}
 }
 
+/*
+ * name: MODULE_FUN_NAME(Set, toArray)
+ * description: put members in @set to array
+ * return value: array to members
+ * args: @set: pointer to set
+ * 		@end: indicator end of array
+ */
 void **MODULE_FUN_NAME(Set, toArray)(T set, void *end)
 {
 	int i;
@@ -264,6 +323,13 @@ void **MODULE_FUN_NAME(Set, toArray)(T set, void *end)
 }
 
 /* return a set, which the member is in t or in s */
+/*
+ * name: MODULE_FUN_NAME(Set, union)
+ * description: put members in @s and @t to a new set
+ * return value: return a set, which the member is in t or in s 
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
 T MODULE_FUN_NAME(Set, union)(T s, T t)
 {
 	int i;
@@ -298,6 +364,13 @@ T MODULE_FUN_NAME(Set, union)(T s, T t)
 }
 
 /* return a set, which the member is in t and in s */
+/*
+ * name: MODULE_FUN_NAME(Set, inter)
+ * description: put members both in @s and @t to a new set
+ * return value: return a set, which the member is in t and in s 
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
 T MODULE_FUN_NAME(Set, inter)(T s, T t)
 {
 	int i;
@@ -346,6 +419,13 @@ T MODULE_FUN_NAME(Set, inter)(T s, T t)
 }
 
 /* return a set, which the member is in t but not in s */
+/*
+ * name: MODULE_FUN_NAME(Set, minus)
+ * description: put members in @s and not in @t to a new set
+ * return value: return a set, which the member is in t but not in s
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
 T MODULE_FUN_NAME(Set, minus)(T t, T s)
 {
 	int i;
@@ -387,6 +467,13 @@ T MODULE_FUN_NAME(Set, minus)(T t, T s)
 }
 
 /* return a set, the member only in s or only in t */
+/*
+ * name: MODULE_FUN_NAME(Set, diff)
+ * description:  put members which only in @s or in @t to a new set
+ * return value: return a set, the member only in s or only in t
+ * args: @s: pointer to set @s
+ * 		 @t: pointer to set @t
+ */
 T MODULE_FUN_NAME(Set, diff)(T s, T t)
 {
 	int i;
