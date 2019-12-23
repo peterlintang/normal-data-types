c6d98c3faab399119184c8a086f4065aec850fcd
linxiaojie <xiaojie.lin@mozi.top>
2019-12-23 16:31:16 +0800

    add comments for bit set

--- include/bit.h
+++ include/bit.h
@@ -18,39 +18,167 @@
 #define T Bit_T
 typedef struct T *T;
 
-// < function 172 >
 
-/* 
- * interface: 
- * description:  
- * args: 
- * return value: 
+/*
+ * name: MODULE_FUN_NAME(Bit, new)
+ * description: create a new bit set
+ * return value: return new bit set
+ * args: @length: total length of bits
  */
-extern T MODULE_FUN_NAME(Bit, new)(int length);
-extern void MODULE_FUN_NAME(Bit, free)(T *set);
+T MODULE_FUN_NAME(Bit, new)(int length);
 
-extern int MODULE_FUN_NAME(Bit, length)(T set);
-extern int MODULE_FUN_NAME(Bit, count)(T set);
+/*
+ * name: MODULE_FUN_NAME(Bit, free)
+ * description: free a allocated bit set
+ * return value: void
+ * args: @length: pointer's pointer to bit set
+ */
+void MODULE_FUN_NAME(Bit, free)(T *set);
 
-extern int MODULE_FUN_NAME(Bit, get)(T set, int n);
-extern int MODULE_FUN_NAME(Bit, put)(T set, int n, int bit);
+/*
+ * name: MODULE_FUN_NAME(Bit, length)
+ * description: get the length of bit set 
+ * return value: length of bit set
+ * args: @set: pointer to bit set
+ */
+int MODULE_FUN_NAME(Bit, length)(T set);
 
-extern void MODULE_FUN_NAME(Bit, clear)(T set, int lo, int hi);
-extern void MODULE_FUN_NAME(Bit, set)(T set, int lo, int hi);
-extern void MODULE_FUN_NAME(Bit, not)(T set, int lo, int hi);
+/*
+ * name: MODULE_FUN_NAME(Bit, count)
+ * description: get the total number of 1 in bit set 
+ * return value: total number bits setted to 1 in bit set
+ * args: @set: pointer to bit set
+ */
+int MODULE_FUN_NAME(Bit, count)(T set);
 
-extern int MODULE_FUN_NAME(Bit, lt)(T s, T t);
-extern int MODULE_FUN_NAME(Bit, eq)(T s, T t);
-extern int MODULE_FUN_NAME(Bit, leq)(T s, T t);
+/*
+ * name: MODULE_FUN_NAME(Bit, get)
+ * description: get the value of @n bit in bit set
+ * return value: get the value of @n bit in bit set
+ * args: @set: pointer to bit set
+ * 		 @n: n bit in bit set
+ */
+int MODULE_FUN_NAME(Bit, get)(T set, int n);
 
-extern void MODULE_FUN_NAME(Bit, map)(T set, 
+/*
+ * name: MODULE_FUN_NAME(Bit, put)
+ * description: put the value of @n bit in bit set to be @bit
+ * return value: return the prev value of @n bit in bit set
+ * args: @set: pointer to bit set
+ * 		 @n: n bit in bit set
+ * 		 @bit: value to set
+ */
+int MODULE_FUN_NAME(Bit, put)(T set, int n, int bit);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, clear)
+ * description: clear the @lo to @hi bits in bit set
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to clear
+ * 		 @hi: the end position of bit set to clear
+ */
+void MODULE_FUN_NAME(Bit, clear)(T set, int lo, int hi);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, set)
+ * description: set the @lo to @hi bits in bit set to be 1
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to set
+ * 		 @hi: the end position of bit set to set
+ */
+void MODULE_FUN_NAME(Bit, set)(T set, int lo, int hi);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, not)
+ * description: set the @lo to @hi bits in bit set to be ^
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to set ^
+ * 		 @hi: the end position of bit set to set ^
+ */
+void MODULE_FUN_NAME(Bit, not)(T set, int lo, int hi);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, map)
+ * description: for each bit in @set, call @apply
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @apply: function to call
+void apply(int n(index of bit), int bit(bit value), void *cl(user's data)),
+ * 		 @cl: user's private data
+ */
+void MODULE_FUN_NAME(Bit, map)(T set, 
 				void apply(int n, int bit, void *cl),
 				void *cl);
+/*
+ * name: MODULE_FUN_NAME(Bit, lt)
+ * description: compare @s and @t, if @s is sub set of @t,
+ * 			return 1, else return 0; 
+ * return value: 1:  @s is sub set of @t,
+ * 				0: @s is not a sub set of @t
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+int MODULE_FUN_NAME(Bit, lt)(T s, T t);
 
-extern T MODULE_FUN_NAME(Bit, union)(T s, T t);
-extern T MODULE_FUN_NAME(Bit, inter)(T s, T t);
-extern T MODULE_FUN_NAME(Bit, minus)(T s, T t);
-extern T MODULE_FUN_NAME(Bit, diff)(T s, T t);
+/*
+ * name: MODULE_FUN_NAME(Bit, eq)
+ * description: compare @s and @t are equal or not
+ * return value: 1: equal; 0 : no equal
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+int MODULE_FUN_NAME(Bit, eq)(T s, T t);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, leq)
+ * description: compare @s and @t, if @s is sub set of @t or @s=@t,
+ * 			return 1, else return 0; 
+ * return value: 1:  @s is sub set of @t, or @s = @t
+ * 				0: @s is not a sub set of @t
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+int MODULE_FUN_NAME(Bit, leq)(T s, T t);
+
+
+/*
+ * name: MODULE_FUN_NAME(Bit, union)
+ * description: @s + @t or @s | @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+T MODULE_FUN_NAME(Bit, union)(T s, T t);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, inter)
+ * description: @s & @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+T MODULE_FUN_NAME(Bit, inter)(T s, T t);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, minus)
+ * description: @s &~ @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+T MODULE_FUN_NAME(Bit, minus)(T s, T t);
+
+/*
+ * name: MODULE_FUN_NAME(Bit, diff)
+ * description: @s ^ @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+T MODULE_FUN_NAME(Bit, diff)(T s, T t);
 
 #undef T
 
--- src/bit.c
+++ src/bit.c
@@ -61,7 +61,12 @@ static T copy(T t)
 	return set;
 }
 
-// < function 203 >
+/*
+ * name: MODULE_FUN_NAME(Bit, new)
+ * description: create a new bit set
+ * return value: return new bit set
+ * args: @length: total length of bits
+ */
 T MODULE_FUN_NAME(Bit, new)(int length)
 {
 	T set = NULL;
@@ -88,6 +93,12 @@ T MODULE_FUN_NAME(Bit, new)(int length)
 	return set;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, free)
+ * description: free a allocated bit set
+ * return value: void
+ * args: @length: pointer's pointer to bit set
+ */
 void MODULE_FUN_NAME(Bit, free)(T *set)
 {
 	assert(set && *set);
@@ -97,6 +108,12 @@ void MODULE_FUN_NAME(Bit, free)(T *set)
 	*set = NULL;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, length)
+ * description: get the length of bit set 
+ * return value: length of bit set
+ * args: @set: pointer to bit set
+ */
 int MODULE_FUN_NAME(Bit, length)(T set)
 {
 	assert(set);
@@ -105,8 +122,11 @@ int MODULE_FUN_NAME(Bit, length)(T set)
 }
 
 /*
- * return the count of 1 in set
- * */
+ * name: MODULE_FUN_NAME(Bit, count)
+ * description: get the total number of 1 in bit set 
+ * return value: total number bits setted to 1 in bit set
+ * args: @set: pointer to bit set
+ */
 int MODULE_FUN_NAME(Bit, count)(T set)
 {
 	unsigned char c = 0;
@@ -150,6 +170,13 @@ int MODULE_FUN_NAME(Bit, count)(T set)
 	return length;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, get)
+ * description: get the value of @n bit in bit set
+ * return value: get the value of @n bit in bit set
+ * args: @set: pointer to bit set
+ * 		 @n: n bit in bit set
+ */
 int MODULE_FUN_NAME(Bit, get)(T set, int n)
 {
 	assert(set);
@@ -159,6 +186,14 @@ int MODULE_FUN_NAME(Bit, get)(T set, int n)
 }
 
 
+/*
+ * name: MODULE_FUN_NAME(Bit, put)
+ * description: put the value of @n bit in bit set to be @bit
+ * return value: return the prev value of @n bit in bit set
+ * args: @set: pointer to bit set
+ * 		 @n: n bit in bit set
+ * 		 @bit: value to set
+ */
 int MODULE_FUN_NAME(Bit, put)(T set, int n, int bit)
 {
 	int prev = 0;
@@ -181,6 +216,14 @@ int MODULE_FUN_NAME(Bit, put)(T set, int n, int bit)
 	return prev;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, set)
+ * description: set the @lo to @hi bits in bit set to be 1
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to set
+ * 		 @hi: the end position of bit set to set
+ */
 void MODULE_FUN_NAME(Bit, set)(T set, int lo, int hi)
 {
 	int i;
@@ -215,6 +258,14 @@ void MODULE_FUN_NAME(Bit, set)(T set, int lo, int hi)
 	}
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, clear)
+ * description: clear the @lo to @hi bits in bit set
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to clear
+ * 		 @hi: the end position of bit set to clear
+ */
 void MODULE_FUN_NAME(Bit, clear)(T set, int lo, int hi)
 {
 	int i;
@@ -247,6 +298,14 @@ void MODULE_FUN_NAME(Bit, clear)(T set, int lo, int hi)
 	}
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, not)
+ * description: set the @lo to @hi bits in bit set to be ^
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @lo: the start position of bit set to set ^
+ * 		 @hi: the end position of bit set to set ^
+ */
 void MODULE_FUN_NAME(Bit, not)(T set, int lo, int hi)
 {
 	int i;
@@ -279,6 +338,15 @@ void MODULE_FUN_NAME(Bit, not)(T set, int lo, int hi)
 	}
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, map)
+ * description: for each bit in @set, call @apply
+ * return value: return void
+ * args: @set: pointer to bit set
+ * 		 @apply: function to call
+void apply(int n(index of bit), int bit(bit value), void *cl(user's data)),
+ * 		 @cl: user's private data
+ */
 void MODULE_FUN_NAME(Bit, map)(T set,
 			void apply(int n, int bit, void *cl),
 			void *cl)
@@ -296,6 +364,13 @@ void MODULE_FUN_NAME(Bit, map)(T set,
 }
 
 
+/*
+ * name: MODULE_FUN_NAME(Bit, eq)
+ * description: compare @s and @t are equal or not
+ * return value: 1: equal; 0 : no equal
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 int MODULE_FUN_NAME(Bit, eq)(T s, T t)
 {
 	int i;
@@ -312,6 +387,15 @@ int MODULE_FUN_NAME(Bit, eq)(T s, T t)
 	return 1;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, leq)
+ * description: compare @s and @t, if @s is sub set of @t or @s=@t,
+ * 			return 1, else return 0; 
+ * return value: 1:  @s is sub set of @t, or @s = @t
+ * 				0: @s is not a sub set of @t
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 int MODULE_FUN_NAME(Bit, leq)(T s, T t)
 {
 	int i;
@@ -328,7 +412,16 @@ int MODULE_FUN_NAME(Bit, leq)(T s, T t)
 	return 1;
 }
 
-int MODULE_FUN_NAME(Bit, lq)(T s, T t)
+/*
+ * name: MODULE_FUN_NAME(Bit, lt)
+ * description: compare @s and @t, if @s is sub set of @t,
+ * 			return 1, else return 0; 
+ * return value: 1:  @s is sub set of @t,
+ * 				0: @s is not a sub set of @t
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
+int MODULE_FUN_NAME(Bit, lt)(T s, T t)
 {
 	int i;
 	int lt = 0;
@@ -347,17 +440,38 @@ int MODULE_FUN_NAME(Bit, lq)(T s, T t)
 	return lt;
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, union)
+ * description: @s + @t or @s | @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 T MODULE_FUN_NAME(Bit, union)(T s, T t)
 {
 	SETOP(copy(t), copy(t), copy(s), |);
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, inter)
+ * description: @s & @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 T MODULE_FUN_NAME(Bit, inter)(T s, T t)
 {
 	SETOP(copy(t), MODULE_FUN_NAME(Bit, new)(t->length), 
 				MODULE_FUN_NAME(Bit, new)(s->length), &);
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, minus)
+ * description: @s &~ @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 T MODULE_FUN_NAME(Bit, minus)(T s, T t)
 {
 	SETOP(MODULE_FUN_NAME(Bit, new)(s->length), 
@@ -365,6 +479,13 @@ T MODULE_FUN_NAME(Bit, minus)(T s, T t)
 				copy(s), & ~);
 }
 
+/*
+ * name: MODULE_FUN_NAME(Bit, diff)
+ * description: @s ^ @t
+ * return value: new bit set
+ * args: @s: pointer to bit set @s
+ * 		 @t: pointer to bit set @t
+ */
 T MODULE_FUN_NAME(Bit, diff)(T s, T t)
 {
 	SETOP(MODULE_FUN_NAME(Bit, new)(s->length), 
