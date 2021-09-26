6c5bf8782c42992330ad1fc9dea6719fad6ef829
lxj <lxj@lyjpbc.com>
2021-09-26 17:50:41 +0800

    remodify ring buf to module fun name

--- include/data/ring_buf.h
+++ include/data/ring_buf.h
@@ -10,25 +10,30 @@
 #ifndef RING_BUF_H
 #define RING_BUF_H
 
+#include "module.h"
 
-struct ring_buf_t;
+#define T Ringbuf_T
+typedef struct T *T;
 
 
-struct ring_buf_t *ring_buf_create(unsigned int num);
 
-int ring_buf_destroy(struct ring_buf_t **ppr);
 
-int ring_buf_is_empty(struct ring_buf_t *r);
+T MODULE_FUN_NAME(Ringbuf, create)(unsigned int num);
 
-int ring_buf_is_full(struct ring_buf_t *r);
+int MODULE_FUN_NAME(Ringbuf, destroy)(T *pr);
 
-int ring_buf_left_space(struct ring_buf_t *r);
+int MODULE_FUN_NAME(Ringbuf, empty)(T r);
 
-int ring_buf_available_data(struct ring_buf_t *r);
+int MODULE_FUN_NAME(Ringbuf, full)(T r);
 
-int ring_buf_put(struct ring_buf_t *r, unsigned char *data, unsigned int len);
+int MODULE_FUN_NAME(Ringbuf, left_space)(T r);
 
-int ring_buf_get(struct ring_buf_t *r, unsigned char *data, unsigned int len);
+int MODULE_FUN_NAME(Ringbuf, available_data)(T r);
 
+int MODULE_FUN_NAME(Ringbuf, put)(T r, unsigned char *data, unsigned int len);
+
+int MODULE_FUN_NAME(Ringbuf, get)(T r, unsigned char *data, unsigned int len);
+
+#undef T
 
 #endif
--- src/data/ring_buf.c
+++ src/data/ring_buf.c
@@ -1,4 +1,11 @@
 
+/*
+ *
+ * 实现环形缓冲区
+ * 在num数组中，当write index + 1 == read index时，
+ * 数组满， 此时数组有一个空元素，所以真正的大小式num-1
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +17,9 @@
 #include "ring_buf.h"
 
 
-struct ring_buf_t {
+#define T Ringbuf_T
+
+struct T {
 	unsigned char *buf;
 	unsigned int buf_max;
 	unsigned int read_index;
@@ -18,7 +27,8 @@ struct ring_buf_t {
 };
 
 
-static int ring_buf_print(struct ring_buf_t *r, int len, char *s)
+
+static int ring_buf_print(T r, int len, char *s)
 {
 #if TEST_RING
 	fprintf(stdout, "buf: %p, max: %d, read: %d, write: %d, %s, len: %d\n",
@@ -29,13 +39,13 @@ static int ring_buf_print(struct ring_buf_t *r, int len, char *s)
 }
 
 
-struct ring_buf_t *ring_buf_create(unsigned int num)
+T MODULE_FUN_NAME(Ringbuf, create)(unsigned int num)
 {
 	assert(num > 0);
 
-	struct ring_buf_t *r = NULL;
+	T r = NULL;
 
-	r = (struct ring_buf_t *)calloc(1, sizeof(*r) + num);
+	r = (T)calloc(1, sizeof(*r) + num);
 	if (r)
 	{
 		r->buf_max = num;
@@ -47,17 +57,17 @@ struct ring_buf_t *ring_buf_create(unsigned int num)
 	return r;
 }
 
-int ring_buf_destroy(struct ring_buf_t **ppr)
+int MODULE_FUN_NAME(Ringbuf, destroy)(T *pr)
 {
-	assert(ppr && *ppr);
+	assert(pr && *pr);
 
-	free(*ppr);
-	*ppr = NULL;
+	free(*pr);
+	*pr = NULL;
 	return 0;
 }
 
 
-int ring_buf_is_empty(struct ring_buf_t *r)
+int MODULE_FUN_NAME(Ringbuf, is_empty)(T r)
 {
 	assert(r);
 
@@ -66,7 +76,7 @@ int ring_buf_is_empty(struct ring_buf_t *r)
 	return 0;
 }
 
-int ring_buf_is_full(struct ring_buf_t *r)
+int MODULE_FUN_NAME(Ringbuf, is_full)(T r)
 {
 	assert(r);
 
@@ -75,7 +85,7 @@ int ring_buf_is_full(struct ring_buf_t *r)
 	return 0;
 }
 
-int ring_buf_left_space(struct ring_buf_t *r)
+int MODULE_FUN_NAME(Ringbuf, left_space)(T r)
 {
 	assert(r);
 
@@ -85,7 +95,7 @@ int ring_buf_left_space(struct ring_buf_t *r)
 		return r->read_index - r->write_index - 1;
 }
 
-int ring_buf_available_data(struct ring_buf_t *r)
+int MODULE_FUN_NAME(Ringbuf, available_data)(T r)
 {
 	assert(r);
 
@@ -99,7 +109,7 @@ int ring_buf_available_data(struct ring_buf_t *r)
 	}
 }
 
-int ring_buf_put(struct ring_buf_t *r, unsigned char *data, unsigned int len)
+int MODULE_FUN_NAME(Ringbuf, put)(T r, unsigned char *data, unsigned int len)
 {
 	int ret = 0;
 	int left = 0;
@@ -108,14 +118,14 @@ int ring_buf_put(struct ring_buf_t *r, unsigned char *data, unsigned int len)
 	assert(data != NULL);
 	assert(len > 0);
 
-	if (ring_buf_is_full(r))
+	if (MODULE_FUN_NAME(Ringbuf, is_full)(r))
 	{
 		return -1;
 	}
 
 	ring_buf_print(r, len, "put b");
 
-	left = ring_buf_left_space(r);
+	left = MODULE_FUN_NAME(Ringbuf, left_space)(r);
 	len = (left >= len ? len : left);
 	ret = len;
 
@@ -138,7 +148,7 @@ int ring_buf_put(struct ring_buf_t *r, unsigned char *data, unsigned int len)
 	return ret;
 }
 
-int ring_buf_get(struct ring_buf_t *r, unsigned char *data, unsigned int len)
+int MODULE_FUN_NAME(Ringbuf, get)(T r, unsigned char *data, unsigned int len)
 {
 	int ret = 0;
 	int available = 0;
@@ -147,14 +157,14 @@ int ring_buf_get(struct ring_buf_t *r, unsigned char *data, unsigned int len)
 	assert(data != NULL);
 	assert(len > 0);
 
-	if (ring_buf_is_empty(r))
+	if (MODULE_FUN_NAME(Ringbuf, is_empty)(r))
 	{
 		return -1;
 	}
 
 	ring_buf_print(r, len, "get b");
 
-	available = ring_buf_available_data(r);
+	available = MODULE_FUN_NAME(Ringbuf, available_data)(r);
 	len = available >= len ? len : available;
 	ret = len;
 
@@ -176,5 +186,146 @@ int ring_buf_get(struct ring_buf_t *r, unsigned char *data, unsigned int len)
 
 	return ret;
 }
+
+
+
+
+/***********************************************************************/
+#if 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+
+
+
+
+/********************************/
+
+static pthread_mutex_t buffer_lock;
+static T r_buf;
+static int quit_flag = 0;
+
+static void SleepMs(int msecs) {
+	struct timespec short_wait;
+	struct timespec remainder;
+	short_wait.tv_sec = msecs / 1000;
+	short_wait.tv_nsec = (msecs % 1000) * 1000 * 1000;
+	nanosleep(&short_wait, &remainder);
+
+}
+
+static void *write_thread(void *arg)
+{
+	int ret = 0;
+	int count = (int )arg;
+	char buf[2048] = { 0 };
+	FILE *fp = NULL;
+
+	fp = fopen("output_write", "w");
+
+	fprintf(stdout, "%s: count: %d\n", __func__, count);
+
+
+	while (quit_flag == 0)
+	{
+
+	for (int i = 0; i < 2048; i++)
+	{
+		if ((i + 1) % 16 == 0)
+			buf[i] = '\n';
+		else
+			buf[i] = 'a' + rand() % 26;
+	}
+		pthread_mutex_lock(&buffer_lock);
+		ret = MODULE_FUN_NAME(Ringbuf, put)(r_buf, buf, 2048);
+		pthread_mutex_unlock(&buffer_lock);
+		if (ret > 0)
+		{
+			fwrite(buf, 1, ret, fp);
+			fflush(fp);
+			count--;
+			if (count == 0) break;
+
+			}
+		else
+		{
+			SleepMs(10);
+		}
+	}
+
+	fclose(fp);
+	fp = NULL;
+	fprintf(stdout, "%s: exit\n", __func__);
+
+	return NULL;
+}
+
+static void *read_thread(void *arg)
+{
+	int ret = 0;
+	char buf[320] = { 0 };
+	FILE *fp = NULL;
+
+	fp = fopen("output_read", "w");
+
+	while (quit_flag == 0)
+	{
+		pthread_mutex_lock(&buffer_lock);
+		ret = MODULE_FUN_NAME(Ringbuf, get)(r_buf, buf, 320);
+		pthread_mutex_unlock(&buffer_lock);
+		if (ret > 0)
+		{
+			fwrite(buf, 1, ret, fp);
+			fflush(fp);
+		}
+	}
+
+	fclose(fp);
+	fp = NULL;
+	fprintf(stdout, "%s: exit\n", __func__);
+
+	return NULL;
+}
+
+/*
+ * ring buf的测试函数，开两个线程，一个往buf丢数据，一个从buf读数据，并将两者的数据保存到文件，然后可以用比较工具看数据是否一致
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	pthread_t pid;
+	int count = atoi(argv[1]);
+
+	r_buf = MODULE_FUN_NAME(Ringbuf, create)(8 * 1024);
+
+	pthread_mutex_init(&buffer_lock, NULL);
+
+	pthread_create(&pid, NULL, read_thread, NULL);
+	pthread_create(&pid, NULL, write_thread, (void *)count);
+
+	while (count != 0)
+	{
+		count--;
+	}
+
+
+	sleep(2);
+	quit_flag = 1;
+
+	pthread_mutex_destroy(&buffer_lock);
+	MODULE_FUN_NAME(Ringbuf, destroy)(&r_buf);
+	fprintf(stdout, "%s: exit\n", __func__);
+
+	return 0;
+}
+
+
+#endif
+
 
 
