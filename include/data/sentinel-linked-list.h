8ab73d7c26e0b3292eace8d35ad8234b4edd5c0b
lxj <lxj@lyjpbc.com>
2021-09-27 11:51:53 +0800

    带哨兵的双向链表实现

--- include/data/sentinel-linked-list.h
+++ include/data/sentinel-linked-list.h
@@ -1,22 +1,114 @@
 
+/*
+ * 文件名称: sentinel-linked-list.h
+ * 功能描述：带哨兵的双向链表
+ * 作者：lxj
+ * 创建时间: 2021-09-26
+ *
+ */
 #ifndef SENTINEL_LINKED_LIST_H
 #define SENTINEL_LINKED_LIST_H
 
-struct item {
-	struct item *prev;
-	struct item *next;
-	void *key;
-};
+#include "module.h"
 
-struct Linked_list {
-	struct item nil;
-};
+#define NODE SenDlinkNode_t
+typedef struct NODE *NODE;
+
+#define T SenDlink_T
+typedef struct T *T;
 
 
-int list_search(struct Linked_list *l, void *key);
-int list_insert(struct Linked_list *l, void *key);
-int list_delete(struct Linked_list *l, void *key);
-void list_print(struct Linked_list *l);
+/**
+ * * _SenDlink_create
+ * *@brief 创建带哨兵的双向链表结构，申请必要的内存;
+ * *@param [in] 无
+ * *@param [out] 无
+ * *@return 是否成功
+ * * - 成功：返回相应的结构指针
+ * * - 其他：失败,返回空指针
+ * */
+T MODULE_FUN_NAME(SenDlink, create)(void);
+
+/**
+ * * _SenDlink_destroy
+ * *@brief 销毁带哨兵的双向链表结构，释放必要的内存;
+ * *@param [in] pl 双向链表头部的指针的指针
+ * *@param [out] 无
+ * *@return 无返回值
+ * * - 成功：返回相应的结构指针
+ * * - 其他：失败,返回空指针
+ * */
+void MODULE_FUN_NAME(SenDlink, destroy)(T *pl);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，根据@key关键字查找;
+ * *@param [in] pl 双向链表头部的指针的指针, key 关键字
+ * *@param [out] 无
+ * *@return 返回值 0 成功
+ * * - 成功：0
+ * * - 其他：失败,
+ * */
+int MODULE_FUN_NAME(SenDlink, search)(T l, void *key);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，将@key关键字插入链表;
+ * *@param [in] pl 双向链表头部的指针的指针, key 关键字
+ * *@param [out] 无
+ * *@return 返回值 0 成功
+ * * - 成功：0
+ * * - 其他：失败,
+ * */
+int MODULE_FUN_NAME(SenDlink, insert)(T l, void *key);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，根据@key关键字删除元素;
+ * *@param [in] pl 双向链表头部的指针的指针, key 关键字
+ * *@param [out] 无
+ * *@return 返回值 0 成功
+ * * - 成功：0
+ * * - 其他：失败,
+ * */
+int MODULE_FUN_NAME(SenDlink, delete)(T l, void *key);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，根据@index关键字删除元素;
+ * *@param [in] pl 双向链表头部的指针的指针, key 关键字
+ * *@param [out] 无
+ * *@return 返回值 0 成功
+ * * - 成功：0
+ * * - 其他：失败,
+ * */
+int MODULE_FUN_NAME(SenDlink, delete_by_index)(T l, unsigned int index);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，统计元素个数;
+ * *@param [in] pl 双向链表头部的指针的指针, 
+ * *@param [out] 无
+ * *@return 返回值链表元素个数
+ * * - 成功：> 0
+ * * - 其他：失败,
+ * */
+unsigned int MODULE_FUN_NAME(SenDlink, count)(T l);
+
+/**
+ * * _SenDlink_search
+ * *@brief 在带哨兵的双向链表结构中，遍历每个元素，并对每个元素调用fun
+ * *@param [in] pl 双向链表头部的指针的指针, fun 用户回调函数
+ * *@param [out] 无
+ * *@return 返回值 0 成功
+ * * - 成功：0
+ * * - 其他：失败, -1
+ * */
+int MODULE_FUN_NAME(SenDlink, map)(T l, int (*fun)(void *arg));
+
+
+#undef NODE
+#undef T
 
 #endif // end of SENTINEL_LINKED_LIST_H
 
--- src/data/sentinel-linked-list.c
+++ src/data/sentinel-linked-list.c
@@ -1,34 +1,93 @@
 
+/*
+ * 文件名称: sentinel-linked-list.c
+ * 功能描述：带哨兵的双向链表
+ * 作者：lxj
+ * 创建时间: 2021-09-26
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #include "sentinel-linked-list.h"
 
+#define NODE SenDlinkNode_t
 
-int list_search(struct Linked_list *l, void *key)
+#define T SenDlink_T
+
+struct NODE {
+	NODE prev;
+	NODE next;
+	void *key;
+};
+
+struct T {
+	struct NODE nil;
+	unsigned int count;
+};
+
+T MODULE_FUN_NAME(SenDlink, create)(void)
 {
-	struct item *x = NULL;
+	T l = NULL;
+
+	l = (T)calloc(1, sizeof(*l));
+	if (l)
+	{
+		l->nil.next = &l->nil;
+		l->nil.prev = &l->nil;
+		l->count = 0;
+	}
+
+	return l;
+}
+
+void MODULE_FUN_NAME(SenDlink, destroy)(T *pl)
+{
+	assert(pl);
+	assert(*pl);
+
+	unsigned int count = MODULE_FUN_NAME(SenDlink, count)(*pl);
+
+	for (unsigned int i = 0; i < count; i++)
+	{
+		MODULE_FUN_NAME(SenDlink, delete_by_index)(*pl, 0);
+	}
+
+	assert((*pl)->nil.next == &((*pl)->nil));
+	assert((*pl)->nil.prev == &((*pl)->nil));
+
+	free(*pl);
+	*pl = NULL;
+}
+
+
+int MODULE_FUN_NAME(SenDlink, search)(T l, void *key)
+{
+	NODE x = NULL;
 
 	assert(l);
+	assert(key);
 
 	x = l->nil.next;
 	while ((x != &(l->nil)) && (x->key != key))
 		x = x->next;
 
 	if (x == NULL)
-		return 0;
+		return -1;
 	else
-		return 1;
+		return 0;
 }
 
-int list_insert(struct Linked_list *l, void *key)
+int MODULE_FUN_NAME(SenDlink, insert)(T l, void *key)
 {
-	struct item *new = NULL;
+	NODE new = NULL;
 
 	assert(l);
+//	assert(key);
 
-	new = (struct item *)calloc(1, sizeof(*new));
+	new = (NODE)calloc(1, sizeof(*new));
 	if (new)
 	{
 		new->key = key;
@@ -36,6 +95,8 @@ int list_insert(struct Linked_list *l, void *key)
 		l->nil.next->prev = new;
 		l->nil.next = new;
 		new->prev = &(l->nil);
+
+		l->count++;
 
 		return 0;
 	}
@@ -45,11 +106,12 @@ int list_insert(struct Linked_list *l, void *key)
 	}
 }
 
-int list_delete(struct Linked_list *l, void *key)
+int MODULE_FUN_NAME(SenDlink, delete)(T l, void *key)
 {
-	struct item *cur = NULL;
+	NODE cur = NULL;
 
 	assert(l);
+//	assert(key);
 
 	for (cur = l->nil.next; cur != &(l->nil); cur = cur->next)
 	{
@@ -64,6 +126,8 @@ int list_delete(struct Linked_list *l, void *key)
 
 		free(cur);
 
+		l->count--;
+
 		return 0;
 	}
 	else
@@ -72,16 +136,64 @@ int list_delete(struct Linked_list *l, void *key)
 	}
 }
 
-void list_print(struct Linked_list *l)
+/**
+ * @index start from 0 to l->count - 1
+ */
+int MODULE_FUN_NAME(SenDlink, delete_by_index)(T l, unsigned int index)
 {
-	struct item *cur = NULL;
+	NODE cur = NULL;
+
+	assert(l);
+	assert(index >= 0 && index < l->count);
+
+	for (cur = l->nil.next; cur != &(l->nil); cur = cur->next)
+	{
+		if (index-- == 0)
+			break;
+	}
+
+	if (cur != &(l->nil))
+	{
+		cur->next->prev = cur->prev;
+		cur->prev->next = cur->next;
+
+		free(cur);
+
+		l->count--;
+
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+unsigned int MODULE_FUN_NAME(SenDlink, count)(T l)
+{
+	assert(l);
+	return l->count;
+}
+
+
+/*
+ * @fun 返回0表示成功，遍历继续；返回非0失败，遍历停止
+ *
+ * return val: error -1; success 0
+ */
+int MODULE_FUN_NAME(SenDlink, map)(T l, int (*fun)(void *arg))
+{
+	NODE cur = NULL;
 
 	assert(l);
 
 	for (cur = l->nil.next; cur != &(l->nil); cur = cur->next)
 	{
-		fprintf(stdout, "key: %p\n", cur->key);
+		if (fun(cur->key) != 0) return -1;
+//		fprintf(stdout, "key: %p\n", cur->key);
 	}
+
+	return 0;
 }
 
 
@@ -90,44 +202,55 @@ void list_print(struct Linked_list *l)
 
 #define TEST_NUM	16
 
+static int print(void *arg)
+{
+	fprintf(stdout, "key: %d\n", (int)arg);
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	struct Linked_list *l = NULL;
+	T l = NULL;
 	int ret = 0;
 
-	l = (struct Linked_list *)calloc(1, sizeof(*l));
+	l = MODULE_FUN_NAME(SenDlink, create)();
 	if (l == NULL)
 	{
 		fprintf(stderr, "no mem\n");
 		return -1;
 	}
 
-	l->nil.next = &l->nil;
-	l->nil.prev = &l->nil;
-
 	for (int i = 0; i < TEST_NUM; i++)
 	{
-		ret = list_insert(l, i);
+		ret = MODULE_FUN_NAME(SenDlink, insert)(l, (void *)i);
 		if (ret == -1)
 			fprintf(stderr, "insert error\n");
 	}
 
-	list_print(l);
+	MODULE_FUN_NAME(SenDlink, map)(l, print);
 
 	for (int i = 0; i < TEST_NUM; i++)
 	{
-		ret = list_delete(l, i);
+		ret = MODULE_FUN_NAME(SenDlink, delete)(l, (void *)i);
 		if (ret == -1)
 			fprintf(stderr, "delete error\n");
+		ret = MODULE_FUN_NAME(SenDlink, insert)(l, (void *)(i + 100));
+		if (ret == -1)
+			fprintf(stderr, "insert error\n");
 	}
 
-	ret = list_delete(l, TEST_NUM);
+	MODULE_FUN_NAME(SenDlink, map)(l, print);
+
+	ret = MODULE_FUN_NAME(SenDlink, delete)(l, (void *)TEST_NUM);
 	if (ret == -1)
 		fprintf(stderr, "delete error\n");
+
+	MODULE_FUN_NAME(SenDlink, destroy)(&l);
 
 	return 0;
 }
 */
+
 
 
 
