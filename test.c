ccd2fd108a11fa907b8390781795a0131714ec22
peterlintang <jiexiaolinhj@163.com>
2021-10-08 21:03:51 +0800

    ap2 test

--- test.c
+++ test.c
@@ -881,7 +881,224 @@ static void test_xp(void)
 		memset(str2, 0, XP_LEN * 2);
 		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
 		MODULE_FUN_NAME(XP, rshift)(XP_LEN, xp2, XP_LEN, xp, 9, 1);
-//		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2) != i >> 9)
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2) != i >> 9)
+			fprintf(stdout, "i: %d, tostr: %s, %s, \n", i,
+							MODULE_FUN_NAME(XP, tostr)(str, XP_LEN * 3, 16, XP_LEN, xp),
+							MODULE_FUN_NAME(XP, tostr)(str2, XP_LEN * 3, 16, XP_LEN, xp2));
+	}
+
+}
+
+static void test_ap2(void)
+{
+#define XP_TEST_NUM	10240
+#define XP_LEN	16
+	int ret = 0;
+	int len = 0;
+	XP_T xp = NULL;
+	unsigned char xp_str[XP_LEN] = { 0 };
+	unsigned char str[XP_LEN * 3] = { 0 };
+	XP_T xp2 = NULL;
+	unsigned char xp2_str[XP_LEN] = { 0 };
+	unsigned char str2[XP_LEN * 3] = { 0 };
+	XP_T sum = NULL;
+	unsigned char sum_str[XP_LEN] = { 0 };
+	XP_T z = NULL;
+	unsigned char z_str[XP_LEN * 2] = { 0 };
+	XP_T q = NULL;
+	unsigned char q_str[XP_LEN] = { 0 };
+	XP_T r = NULL;
+	unsigned char r_str[XP_LEN] = { 0 };
+	XP_T tmp = NULL;
+	unsigned char tmp_str[XP_LEN * 2 + 2] = { 0 };
+
+	xp = xp_str;
+	xp2 = xp2_str;
+	sum = sum_str;
+	z = z_str;
+
+	q = q_str;
+	r = r_str;
+	tmp = tmp_str;
+
+	fprintf(stdout, "first\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, xp) != i)
+			fprintf(stdout, "i: %d, toint: %lu\n", i, MODULE_FUN_NAME(XP, toint)(XP_LEN, xp));
+	}
+
+	fprintf(stdout, "second add\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, add)(XP_LEN, sum, xp, xp2, 0);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != i + i + 1)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+
+	fprintf(stdout, "third sub\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, sub)(XP_LEN, sum, xp2, xp, 0);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != 1)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+
+	fprintf(stdout, "four mul\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(z, 0, XP_LEN * 2);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, mul)(z, XP_LEN, xp2, XP_LEN, xp);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN * 2, z) != ((unsigned long)i) * (unsigned long)(i + 1))
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN * 2, z));
+	}
+
+	fprintf(stdout, "four div\n");
+	MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, 9);
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(z, 0, XP_LEN * 2);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i);
+		MODULE_FUN_NAME(XP, div)(XP_LEN, q, xp2, XP_LEN, xp, r, tmp);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN , q) != (i) / (9) 
+			|| MODULE_FUN_NAME(XP, toint)(XP_LEN, r) != i % 9)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, q),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, r));
+	}
+	
+	fprintf(stdout, "five sum\n");
+	for (int j = 1; j < 256; j++)
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, sum)(XP_LEN, sum, xp, j);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != i + j)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+
+	fprintf(stdout, "six diff\n");
+	for (int j = 1; j < 256; j++)
+	{
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i + j);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, diff)(XP_LEN, sum, xp, j);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != i)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+	}
+
+	fprintf(stdout, "seven product\n");
+	for (int j = 1; j < 256; j++)
+	{
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i + 1);
+		MODULE_FUN_NAME(XP, product)(XP_LEN, sum, xp, j);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != (unsigned long)i * (unsigned long)j)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i, 
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+	}
+
+	fprintf(stdout, "eight quotient\n");
+	for (int j = 1; j < 256; j++)
+	{
+	int ret = 0;
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		ret = MODULE_FUN_NAME(XP, quotient)(XP_LEN, sum, xp, j);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, sum) != i / j 
+			|| ret != i % j)
+			fprintf(stdout, "i: %d, j: %d, toint: %lu, %lu\n", i, j,
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+	}
+
+	fprintf(stdout, "nine cmp\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp2, i);
+		if (MODULE_FUN_NAME(XP, cmp)(XP_LEN, xp, xp2) != 0)
+			fprintf(stdout, "i: %d, toint: %lu, %lu\n", i,
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2));
+	}
+
+	fprintf(stdout, "ten neg\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, neg)(XP_LEN, xp2, xp, 0);
+		MODULE_FUN_NAME(XP, neg)(XP_LEN, sum, xp2, 0);
+		if (MODULE_FUN_NAME(XP, cmp)(XP_LEN, xp, sum) != 0)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i,
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+
+	fprintf(stdout, "eleven lshift\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(sum, 0, XP_LEN);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, lshift)(XP_LEN, xp2, XP_LEN, xp, 2, 0);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2) != i << 2)
+			fprintf(stdout, "i: %d, toint: %lu, %lu, %lu\n", i,
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2),
+							MODULE_FUN_NAME(XP, toint)(XP_LEN, sum));
+	}
+
+	fprintf(stdout, "twelve rshift\n");
+	for (int i = 0; i < XP_TEST_NUM; i++)
+	{
+		memset(xp, 0, XP_LEN);
+		memset(xp2, 0, XP_LEN);
+		memset(str, 0, XP_LEN * 2);
+		memset(str2, 0, XP_LEN * 2);
+		MODULE_FUN_NAME(XP, fromint)(XP_LEN, xp, i);
+		MODULE_FUN_NAME(XP, rshift)(XP_LEN, xp2, XP_LEN, xp, 9, 1);
+		if (MODULE_FUN_NAME(XP, toint)(XP_LEN, xp2) != i >> 9)
 			fprintf(stdout, "i: %d, tostr: %s, %s, \n", i,
 							MODULE_FUN_NAME(XP, tostr)(str, XP_LEN * 3, 16, XP_LEN, xp),
 							MODULE_FUN_NAME(XP, tostr)(str2, XP_LEN * 3, 16, XP_LEN, xp2));
@@ -912,6 +1129,7 @@ struct test_routine my_test_routines[] =
 //		{test_ring, "ring"},
 //		{test_str, "string"},
 		{test_xp, "xp"},
+		{test_ap2, "ap2"},
 		{NULL,NULL},
 };
 
