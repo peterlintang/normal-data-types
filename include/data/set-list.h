eccf61520e2f1a28ab7eefe619c65ef95e0df9d9
peterlintang <jiexiaolinhj@163.com>
2021-10-24 18:06:31 +0800

    用链表实现集合

--- include/data/set-list.h
+++ include/data/set-list.h
@@ -1,47 +1,97 @@
 
-#ifndef MY_SET_LIST_H__
-#define MY_SET_LIST_H__
-
-
 /*
- * use list to implement set (bu xiang jiao)
+ * 用双向链表实现集合数据结构
+ * 集合中的元素应该唯一
  */
 
-#define SET_ERROR	-1
+#ifndef CI_SET_LIST_H__
+#define CI_SET_LIST_H__
 
-struct set;
+#include "module.h"
+#include "dlist.h"
 
-struct set_item {
-	struct set *set;
-	struct set_item *next;
-//	void *p;
-	int p;
-};
+#define T SetL_T
 
-struct set {
-	struct set_item *head;
-	struct set_item *tail;
+typedef struct T *T;
+
+struct T {
+	ListD_T l;
 	int n;
-	struct set *next;
-	struct set *prev;
+	int (*cmp)(void *priv, void *arg);
 };
-
-struct sets {
-	struct set *head;
-	struct set *tail;
-	int n;
-};
-
-int set_make(struct sets *sets, int x);
-
-int set_find(struct sets *sets, int x);
 
 /*
- *
- *  union operation, the @sets must have 2 sets in the list
- **/
-int set_union(struct sets *sets, int x, int y);
+ * 新建一个链表集合，成功返回指针，失败NULL
+ */
+T MODULE_FUN_NAME(SetL, new)(int (*cmp)(void *, void *));
 
-void set_print(struct sets *sets);
+/*
+ * 释放一个链表集合内容
+ */
+void MODULE_FUN_NAME(SetL, free)(T *setp);
 
-#endif // MY_SET_LIST_H__
+/*
+ * 将新元素priv添加到集合set中
+ * 成功返回0，失败-1
+ */
+int MODULE_FUN_NAME(SetL, add)(T set, void *priv);
+
+/*
+ * 将元素priv从集合set中删除
+ * 成功返回指针，失败NULL
+ */
+void *MODULE_FUN_NAME(SetL, remove)(T set, void *priv);
+
+/*
+ * 集合set中查找元素,
+ * 成功返回指针，失败NULL
+ */
+void *MODULE_FUN_NAME(SetL, search)(T set, void *priv);
+
+/*
+ * 判断priv是否在集合set中，
+ * 是返回1，不是0
+ */
+int MODULE_FUN_NAME(SetL, isMember)(T set, void *priv);
+
+/*
+ * 返回集合set中元素的数目
+ */
+int MODULE_FUN_NAME(SetL, count)(T set);
+
+/*
+ * 根据索引返回集合中的第index个元素
+ * 超出元素个数返回NULL,成功返回指针
+ */
+void *MODULE_FUN_NAME(SetL, get)(T set, int index);
+
+/********************集合高级操作*******************/
+/*
+ * 新的集合为s U t，失败NULL
+ * 集合s与t的并集
+ */
+T MODULE_FUN_NAME(SetL, union)(T s, T t);
+
+/*
+ * 新的集合为s * t，失败NULL
+ * 集合s与t的集
+ */
+T MODULE_FUN_NAME(SetL, inter)(T s, T t);
+
+/*
+ * 新的集合为s / t，失败NULL
+ * 新集合的元素：仅在集合s,或仅在集合t
+ */
+T MODULE_FUN_NAME(SetL, diff)(T s, T t);
+
+/*
+ * 新的集合为s - t，失败NULL
+ * 新集合的元素：在集合s,但不在t
+ */
+T MODULE_FUN_NAME(SetL, minus)(T s, T t);
+
+#undef T
+
+#endif // CI_SET_LIST_H__
+
+
--- src/data/set-list.c
+++ src/data/set-list.c
@@ -1,13 +1,95 @@
 
+/*
+ * 用双向链表实现集合数据结构
+ * 集合中的元素应该唯一
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #include "set-list.h"
 
+#define T SetL_T
+
+
+struct T {
+	ListD_T l;
+	int n;
+	int (*cmp)(void *priv, void *arg);
+};
+
 /*
- * use list to implement set (bu xiang jiao)
+ * 新建一个链表集合，成功返回指针，失败NULL
  */
+T MODULE_FUN_NAME(SetL, new)(int (*cmp)(void *, void *));
+
+/*
+ * 释放一个链表集合内容
+ */
+void MODULE_FUN_NAME(SetL, free)(T *setp);
+
+/*
+ * 将新元素priv添加到集合set中
+ * 成功返回0，失败-1
+ */
+int MODULE_FUN_NAME(SetL, add)(T set, void *priv);
+
+/*
+ * 将元素priv从集合set中删除
+ * 成功返回指针，失败NULL
+ */
+void *MODULE_FUN_NAME(SetL, remove)(T set, void *priv);
+
+/*
+ * 集合set中查找元素,
+ * 成功返回指针，失败NULL
+ */
+void *MODULE_FUN_NAME(SetL, search)(T set, void *priv);
+
+/*
+ * 判断priv是否在集合set中，
+ * 是返回1，不是0
+ */
+int MODULE_FUN_NAME(SetL, isMember)(T set, void *priv);
+
+/*
+ * 返回集合set中元素的数目
+ */
+int MODULE_FUN_NAME(SetL, count)(T set);
+
+/*
+ * 根据索引返回集合中的第index个元素
+ * 超出元素个数返回NULL,成功返回指针
+ */
+void *MODULE_FUN_NAME(SetL, get)(T set, int index);
+
+/********************集合高级操作*******************/
+/*
+ * 新的集合为s U t，失败NULL
+ * 集合s与t的并集
+ */
+T MODULE_FUN_NAME(SetL, union)(T s, T t);
+
+/*
+ * 新的集合为s * t，失败NULL
+ * 集合s与t的集
+ */
+T MODULE_FUN_NAME(SetL, inter)(T s, T t);
+
+/*
+ * 新的集合为s / t，失败NULL
+ * 新集合的元素：仅在集合s,或仅在集合t
+ */
+T MODULE_FUN_NAME(SetL, diff)(T s, T t);
+
+/*
+ * 新的集合为s - t，失败NULL
+ * 新集合的元素：在集合s,但不在t
+ */
+T MODULE_FUN_NAME(SetL, minus)(T s, T t);
+
+
+
 
 
 static void _set_destroy(struct set **setp)
@@ -401,8 +483,5 @@ int main(int argc, char *argv[])
 	return 0;
 }
  */
-
-
-
 
 
