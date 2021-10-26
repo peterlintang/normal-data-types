cfc5b2a5e4492467e7010b7cf12a8301ffef939d
peterlintang <jiexiaolinhj@163.com>
2021-10-26 21:59:52 +0800

    连通分量，未完成

---
+++ include/util/components.h
@@ -0,0 +1,35 @@
+
+/*
+ * 计算图的连通分量
+ * 用set-list实现
+ */
+
+#include "dlist.h"
+#include "gve_array.h"
+#include "set-list.h"
+
+struct node {
+	int v;
+};
+
+struct edge_ext {
+	int value;
+};
+
+
+/*
+ * 计算图g的连通分量,图最后的连通分量保存在链表sets中，
+ * 链表中每一个元素代表一个连通分量
+ * g: 有节点信息，边信息的图
+ * sets: 空链表
+ */
+void components_connected(ListD_T sets, GraphA_T g);
+
+/*
+ * 判断x与y是否在同一个连通分量中，即判断x与y是否连通
+ * sets: 连通分量链表
+ * x:
+ * y:
+ */
+int components_same(ListD_T sets, void *x, void *y);
+
--- src/util/components.c
+++ src/util/components.c
@@ -7,307 +7,94 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include "dlist.h"
+#include "gve_array.h"
 #include "set-list.h"
+#include "components.h"
 
 
-
-static void _set_destroy(struct set **setp)
+static void *get_graphA(FILE *fp)
 {
-	assert(setp);
-	assert(*setp);
+	GraphA_T g = NULL;
+	int node_item = 0;
+	int edge_item = 0;
 
-	free(*setp);
-	*setp = NULL;
+	scanf(fp, "%d", &node_item);
+	scanf(fp, "%d", &edge_item);
+	fprintf(stdout, "%s: node: %d, edge: %d\n", __func__, node_item, edge_item);
+
+	g = MODULE_FUN_NAME(GraphA, create)(sizeof(struct node), node_item, sizeof(struct edge_ext), edge_item, NULL);
+
+	return g;
 }
 
-static struct set *_set_make(int x)
+static int get_node(FILE *fp, void *g_p, void *n_p)
 {
-	struct set *set = NULL;
-	struct set_item *item = NULL;
+	int v = 0;
+	struct node *node = (struct node *)n_p;
+	GraphA_T g = (GraphA_T)g_p;
 
-	assert(x);
+	scanf(fp, "%d", &v);
 
-	set = (struct set *)calloc(1, sizeof(*set));
-	if (set)
+	node->v = v;
+
+	return 0;
+}
+
+static int cmp(void *arg, void *priv)
+{
+	struct node *node = (struct node *)arg;
+	int num = (int)priv;
+
+	if (num == node->v) return 0;
+	else return 1;
+}
+
+static int get_edge(FILE *fp, void *g_p, void *e_p)
+{
+	int v = 0;
+	int u = 0;
+	int value = 0;
+	struct node *n_v = NULL;
+	struct node *n_u = NULL;
+	struct edge_ext *ext = NULL;
+	GraphA_T g = (GraphA_T)g_p;
+	EdgeA_T edge = (EdgeA_T)e_p;
+
+	scanf(fp, "%d,%d,%d", &v, &u, &value);
+
+	n_v = MODULE_FUN_NAME(GraphA, VnodeSearch)(g, cmp, (void *)v);
+	n_u = MODULE_FUN_NAME(GraphA, VnodeSearch)(g, cmp, (void *)u);
+	if (n_v == NULL || n_u == NULL)
 	{
-		item = (struct set_item *)calloc(1, sizeof(*item));
-		if (item)
-		{
-			set->head = item;
-			set->tail = item;
-			set->next = NULL;
-			set->prev = NULL;
-			set->n = 1;
-			item->p = x;
-			item->set = set;
-			item->next = NULL;
-		}
-		else
-		{
-			free(set);
-			set = NULL;
-		}
+		fprintf(stdout, "search %d, %d failed %p, %p\n", n_v, n_u);
+		return -1;
 	}
 
-	return set;
+	MODULE_FUN_NAME(GraphA, EdgeSetVnodes)(edge, (void *)n_v, (void *)n_u, NULL, NULL);
+	ext = (struct edge_ext *)MODULE_FUN_NAME(GraphA, EdgeGetPriv)((void *)edge);
+	ext->value = value;
+	return 0;
+}
+
+GraphA_T components_create_graph(char *fliename)
+{
+	assert(filename);
+
+	GraphA_T g = NULL;
+
+	g = GA_create(filename, get_graphA, get_edge, get_node);
+
+	return g;
 }
 
 /*
- * attention: user free @x and @y
+ * 计算图g的连通分量,图最后的连通分量保存在链表sets中，
+ * 链表中每一个元素代表一个连通分量
+ * g: 有节点信息，边信息的图
+ * sets: 空链表
  */
-static struct set *_set_union(struct set *x, struct set *y)
-{
-	struct set *z = NULL;
-	struct set_item *item = NULL;
-
-	assert(x);
-	assert(y);
-
-	/* not empty set */
-	assert(x->head);
-	assert(y->head);
-
-	z = (struct set *)calloc(1, sizeof(*z));
-	if (NULL == z)
-	{
-		return NULL;
-	}
-
-	z->head = x->head;
-	z->tail = x->tail;
-	z->n = x->n;
-
-	do 
-	{
-		item = y->head;
-		y->head = item->next;
-		y->n -= 1;
-
-		z->tail->next = item;
-		item->next = NULL;
-		z->tail = item;
-		z->n += 1;
-
-	} while (y->head != NULL);
-
-	x->head = NULL;
-	x->tail = NULL;
-	x->n = 0;
-
-	y->head = NULL;
-	y->tail = NULL;
-	y->n = 0;
-
-	for (item = z->head; item != NULL; item = item->next)
-	{
-		item->set = z;
-	}
-
-	return z;
-}
-
-static struct set *_set_find(struct set *set, int x)
-{
-	struct set_item *item = NULL;
-
-	assert(set);
-	assert(x);
-
-	for (item = set->head; item != NULL; item = item->next)
-	{
-		if (item->p == x)
-			return set;
-	}
-
-	return NULL;
-}
-
-
-/*****************************/
-int set_make(struct sets *sets, int x)
-{
-	struct set *set = NULL;
-
-	assert(sets);
-
-	set = _set_make(x);
-	if (set)
-	{
-		set->prev = NULL;
-		set->next = NULL;
-
-		if (sets->head == NULL)
-		{
-			sets->head = set;
-			sets->tail = set;
-			sets->n = 1;
-		}
-		else
-		{
-			sets->tail->next = set;
-			set->prev = sets->tail;
-			sets->tail = set;
-			sets->n += 1;
-		}
-
-		return x;
-	}
-	else
-	{
-		fprintf(stderr, "%s: no mem\n", __func__);
-		return SET_ERROR;
-	}
-}
-
-int set_find(struct sets *sets, int x)
-{
-	struct set *set = NULL;
-
-	assert(sets);
-
-	for (set = sets->head; set != NULL; set = set->next)
-	{
-		if (_set_find(set, x))
-			return set->head->p;
-	}
-
-	return SET_ERROR;
-}
-
-/*
- *
- *  union operation, the @sets must have 2 sets in the list
- **/
-int set_union(struct sets *sets, int x, int y)
-{
-	struct set *set_x = NULL;
-	struct set *set_y = NULL;
-	struct set *set = NULL;
-
-	assert(sets);
-
-	for (set = sets->head; set != NULL; set = set->next)
-	{
-		if (set_x == NULL)
-			set_x = _set_find(set, x);
-		if (set_y == NULL)
-			set_y = _set_find(set, y);
-
-		if ((set_x != NULL) && (set_y != NULL))
-			break;
-	}
-
-	assert(set_x);
-	assert(set_y);
-	assert(set_x != set_y);
-
-	set = _set_union(set_x, set_y);
-	if (NULL == set)
-	{
-		fprintf(stderr, "%s: no mem", __func__);
-		return SET_ERROR;
-	}
-
-	if (set_x == sets->head)
-	{
-		sets->head = set_x->next;
-		if (sets->head)
-			sets->head->prev = NULL;
-	}
-	else if (set_x == sets->tail)
-	{
-		sets->tail = set_x->prev;
-		if (sets->tail)
-			sets->tail->next = NULL;
-	}
-	else
-	{
-		set_x->prev->next = set_x->next;
-		set_x->next->prev = set_x->prev;
-	}
-	set_x->prev = NULL;
-	set_x->next = NULL;
-	sets->n -= 1;
-
-	if (set_y == sets->head)
-	{
-		sets->head = set_y->next;
-		if (sets->head)
-			sets->head->prev = NULL;
-	}
-	else if (set_y == sets->tail)
-	{
-		sets->tail = set_y->prev;
-		if (sets->tail)
-			sets->tail->next = NULL;
-	}
-	else
-	{
-		set_y->prev->next = set_y->next;
-		set_y->next->prev = set_y->prev;
-	}
-	set_y->prev = NULL;
-	set_y->next = NULL;
-	sets->n -= 1;
-
-	_set_destroy(&set_x);
-	_set_destroy(&set_y);
-
-	if (sets->n == 0)
-	{
-		sets->head = set;
-		sets->tail = set;
-		set->next = NULL;
-		set->prev = NULL;
-	}
-	else
-	{
-		sets->tail->next = set;
-		set->prev = sets->tail;
-		sets->tail = set;
-		set->next = NULL;
-	}
-	sets->n += 1;
-
-	return set->head->p;
-}
-
-void set_print(struct sets *sets)
-{
-	struct set *set = NULL;
-	struct set_item *item = NULL;
-
-	assert(sets);
-
-	for (set = sets->head; set != NULL; set = set->next)
-	{
-		for (item = set->head; item != NULL; item = item->next)
-		{
-			fprintf(stdout, " %d, %p;", item->p, item->set);
-		}
-		fprintf(stdout, "\n");
-	}
-}
-
-/***************************/
-
-struct V {
-	int v;
-};
-
-struct E {
-	int u;
-	int v;
-};
-
-struct G {
-	struct V *vs;
-	int vs_num;
-	struct E *es;
-	int es_num;
-};
-
-void connected_components(struct sets *sets, struct G *g)
+void components_connected(ListD_T sets, GraphA_T g)
 {
 	for (int i = 0; i < g->vs_num; i++)
 	{
@@ -323,7 +110,7 @@ void connected_components(struct sets *sets, struct G *g)
 	}
 }
 
-int same_components(struct sets *sets, int x, int y)
+int components_same(struct sets *sets, int x, int y)
 {
 	int x_r = 0;
 	int y_r = 0;
@@ -336,73 +123,4 @@ int same_components(struct sets *sets, int x, int y)
 	else
 		return 0;
 }
-
-
-/*
- * test code
-int main(int argc, char *argv[])
-{
-	struct sets sets;
-	struct G g;
-	struct V *vs = NULL;
-	struct E *es = NULL;
-	int x;
-	int y;
-
-	if (argc != 3)
-	{
-		fprintf(stderr, "usage: %s num1 num2\n", argv[0]);
-		exit(0);
-	}
-
-
-	vs = (struct V *)calloc(10, sizeof(*vs));
-	es = (struct E *)calloc(7, sizeof(*es));
-
-	for (int i = 0; i < 10; i++)
-	{
-		vs[i].v = i + 1;
-	}
-
-	es[0].u = 1;
-	es[0].v = 2;
-	es[1].u = 1;
-	es[1].v = 3;
-	es[2].u = 2;
-	es[2].v = 3;
-	es[3].u = 2;
-	es[3].v = 4;
-	es[4].u = 5;
-	es[4].v = 6;
-	es[5].u = 5;
-	es[5].v = 7;
-	es[6].u = 8;
-	es[6].v = 9;
-
-	g.vs = vs;
-	g.vs_num = 10;
-	g.es = es;
-	g.es_num = 7;
-
-	sets.head = NULL;
-	sets.tail = NULL;
-	sets.n = 0;
-
-	connected_components(&sets, &g);
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	if (same_components(&sets, x, y))
-		fprintf(stdout, "%d, %d is in the same set\n", x, y);
-	else
-		fprintf(stdout, "%d, %d is not in the same set\n", x, y);
-
-	set_print(&sets);
-
-	return 0;
-}
- */
-
-
-
 
