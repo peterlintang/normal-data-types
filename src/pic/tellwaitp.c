d4c6bfb2d7e8b6d94df5f2cdb47443acee4448f9
peterlintang <jiexiaolinhj@163.com>
2021-10-28 21:26:24 +0800

    commit tell wait pipe

--- src/pic/tellwaitp.c
+++ src/pic/tellwaitp.c
@@ -1,26 +1,26 @@
 /*
  * 父子进程同步原语
  */
-#include "apue.h"
+#include "tellwaitp.h"
 
 static int pfd1[2], pfd2[2];
 
 void
-TELL_WAIT(void)
+TELL_WAIT_P(void)
 {
 	if (pipe(pfd1) < 0 || pipe(pfd2) < 0)
 		err_sys("pipe error");
 }
 
 void
-TELL_PARENT(pid_t pid)
+TELL_PARENT_P(pid_t pid)
 {
 	if (write(pfd2[1], "c", 1) != 1)
 		err_sys("pipe error");
 }
 
 void
-WAIT_PARENT(void)
+WAIT_PARENT_P(void)
 {
 	char 	c;
 
@@ -32,14 +32,14 @@ WAIT_PARENT(void)
 }
 
 void
-TELL_CHILD(pid_t pid)
+TELL_CHILD_P(pid_t pid)
 {
 	if (write(pfd1[1], "p", 1) != 1)
 		err_sys("write error");
 }
 
 void
-WAIT_CHILD(void)
+WAIT_CHILD_P(void)
 {
 	char	c;
 
@@ -49,3 +49,5 @@ WAIT_CHILD(void)
 	if (c != 'c')
 		err_quit("WAIT_CHILD: incorrect data");
 }
+
+
