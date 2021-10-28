d4c67ed07fe794def7d7077e086e19b7fc8827ae
peterlintang <jiexiaolinhj@163.com>
2021-10-28 23:00:16 +0800

    快速排序测试

---
+++ test/quicksort-test.c
@@ -0,0 +1,51 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "quicksort.h"
+
+static int cmp(void *x, void *y)
+{
+	int x_i = (int)x;
+	int y_i = (int)y;
+
+	if (x_i > y_i) return 1;
+	else if (x_i < y_i) return -1;
+	else return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	void **a = NULL;
+	int len = 1024;
+	int value = 0;
+
+	if (argc == 2)
+		len = atoi(argv[1]);
+
+	a = (void **)calloc(len, sizeof(void *));
+	if (a == NULL)
+	{
+		fprintf(stdout, "no mem %d\n", len);
+		return -1;
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		value = random() % len + 1;
+//		value = i + 1;
+		a[i] = (void *)value;
+	//	fprintf(stdout, "%p\n", a[i]);
+	}
+
+//	fprintf(stdout, "quick sort\n");
+	quicksort(a, cmp, 0, len - 1);
+
+	for (int i = 0; i < len; i++)
+	{
+		fprintf(stdout, "%p\n", a[i]);
+	}
+
+	return 0;
+}
+
