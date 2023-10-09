174000bafb39c1874b2f6fabe4685dfaa11852d3
linxj <linxj>
2023-10-09 11:54:07 +0800

    add crc8 test

--- bak/crc_test.c
+++ bak/crc_test.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
 	int ret = 0;
 	uint32_t crc = 0;
+	uint8_t crc8 = 0;
 	long length = 0;
 	FILE *fp = NULL;
 	unsigned char *buff = NULL;
@@ -22,8 +23,9 @@ int main(int argc, char *argv[])
 	fclose(fp);
 	fp = NULL;
 	crc = crc_32(buff, ret);
+	crc8 = crc_8(buff, ret);
 
-	fprintf(stdout, "%0x\n", crc);
+	fprintf(stdout, "%0x, %0x\n", crc, crc8);
 	free(buff);
 	return 0;
 }
