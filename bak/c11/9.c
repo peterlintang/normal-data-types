3fda8850573d92cfb856a0985e1b6ade4a77d775
linxj <linxj>
2024-03-25 14:55:14 +0800

    add 9 10 source file

---
+++ bak/c11/9.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define hash_hash # ## #
+#define TO_STRING(T)	#T
+#define NUM_B		9
+#define IN_BETWEEN(a) TO_STRING(a)
+#define COMBINE(X,T)	X##T
+
+int main(int argc, char *argv[])
+{
+	printf("hello world: %s\n", TO_STRING(balidy));
+	printf("hello world: %d\n", COMBINE(NUM_, B));
+	printf("hello world: %s\n", COMBINE(TO_, STRING)(111));
+	printf("hello world: %s\n", TO_STRING((111 hash_hash dd)));
+	printf("hello world: %s\n", IN_BETWEEN((111 hash_hash dd)));
+	return 0;
+}
+
