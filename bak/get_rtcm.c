5ea3fd40ec7ea1f8d76fa87bd61d7da018efd0a9
peterlintang <jiexiaolinhj@163.com>
2025-08-01 10:09:31 +0800

    Add files via upload

--- bak/get_rtcm.c
+++ bak/get_rtcm.c
@@ -34,28 +34,29 @@ static void get_rtcm_packet(unsigned char *data, int len)
 		}
 		else
 		{
-                        if (rtcm_data_cur >= RTCM_DATA_MAX)
-                        {
-                                printf("rtcm over room %02x %02x %02x, reset\n", rtcm_data[0], rtcm_data[1], rtcm_data[2]);
-                                rtcm_data_len = -1;
-                                rtcm_data_cur = 0;
-                                continue;
-                        }
-			
-			rtcm_data[rtcm_data_cur++] = data[i];
-			if (rtcm_data_cur == 3)
+			if (rtcm_data_cur >= RTCM_DATA_MAX)
 			{
-				if (rtcm_data[0] == 0xD3 
-					&& ((rtcm_data[1] & 0xfc) == 0x00))
+				printf("rtcm over room %02x %02x %02x, reset\n", rtcm_data[0], rtcm_data[1], rtcm_data[2]);
+				rtcm_data_len = -1;
+				rtcm_data_cur = 0;
+			}
+			else
+			{
+				rtcm_data[rtcm_data_cur++] = data[i];
+				if (rtcm_data_cur == 3)
 				{
-					rtcm_data_len = ((rtcm_data[1] & 0x03) << 8) | rtcm_data[2];
-					printf("rtcm data len: %d\n", rtcm_data_len);
-				}
-				else
-				{
-					printf("rtcm error head %02x %02x %02x, reset\n", rtcm_data[0], rtcm_data[1], rtcm_data[2]);
-					rtcm_data_len = -1;
-					rtcm_data_cur = 0;
+					if (rtcm_data[0] == 0xD3 
+						&& ((rtcm_data[1] & 0xfc) == 0x00))
+					{
+						rtcm_data_len = ((rtcm_data[1] & 0x03) << 8) | rtcm_data[2];
+						printf("rtcm data len: %d\n", rtcm_data_len);
+					}
+					else
+					{
+						printf("rtcm error head %02x %02x %02x, reset\n", rtcm_data[0], rtcm_data[1], rtcm_data[2]);
+						rtcm_data_len = -1;
+						rtcm_data_cur = 0;
+					}
 				}
 			}
 		}
